b4e3d04be851c1d848e493445bdd1cc9a5d0f249
fvisconti <france.visconti@gmail.com>
2016-06-15 11:23:56 +0200

    changed color palette creation and fixed scale issue

--- parallelPlots.R
+++ parallelPlots.R
@@ -1,16 +1,17 @@
 args<-commandArgs(TRUE)
-# takes:
-# an input csv file, with ";" separator, where the first column is named "ID", and the last is "type"
-# the number of column to be viewed as input variables
-# the number of column to be viewed as output variables
-# the column number to be used as filter, then the extremes (low and up) to filter the data
-# a second column number to be used as filter, then the extremes (low and up) to filter the data
-# a 0/1 flag to filter data upon the "type" column
-# width, height and name of the ouput png file
-# the column upon wich grouping the lines in colors
-# how many intervals (then colors) to use
-# the working directory
+# modifica 04/05/2016 - Francesco
+# vengono usate massimo due variabili per il filtraggio, con la seguente logica:
+# alla prima chiamata dello script vengono passati i valori di filtro per una sola
+# variabile, per la seconda non sulla GUI non viene proposto il campo d'inserimento
+# e viene passata una tripletta di zeri.
+# Generato il primo grafico con filtro, viene acceso il form per la seconda variabile
+# e bloccato l'inserimento per i filtri della prima.
+# Il tasto reset azzera tutte le selezioni e riaccende i campi inserimento per la prima
+# variabile di filtro.
+
 ## args format  (input_file.csv, n_col_input, ncol_output, col_filter1, low1, up1, col_filter2, low2, up2, only_DoE_0_1, width_px, height_px, output_file.png, group_col, group_intervals, working_dir)
+## args <-c("benchmark29.csv","4","2","7","1.25","1.29", "6", "0.08843", "0.09438", "1","1000","600","parallel3.png", 6, 20, "~/R/RBFAero/Prod/v1.1/") 
+## args <-c("benchmark29.csv","4","2","0","0","0", "0", "0", "0", "1","1000","600","parallel4.png", 6, 20, "~/R/RBFAero/Prod/v1.1/") 
 
 setwd(args[length(args)]) #set wdir
 args <- args[-length(args)] #rm wdir entry in args
@@ -18,13 +19,15 @@ args <- args[-length(args)] #rm wdir entry in args
 require(MASS)
 require(dplyr)
 
-onlyDoe <- args[10] %>% as.numeric() # 1 if onlyDoe
-lfilter <- args[4] %>% as.numeric()  # 0 if the dataset has not to be filtered
+# variabile logica, 1 se solo doe
+onlyDoe <- args[10] %>% as.numeric()
+# variabile logica, 0 se non viene filtrato il dataset
+lfilter <- args[4] %>% as.numeric()
 lfilter2 <- args[7] %>% as.numeric()
 
-# just header in dd
+# seleziono solo header in dd
 dd <-args[1] %>% read.csv(sep=";") %>% as.tbl() %>% names()
-# if there's no filter, use "ID" column
+# se non c'e colonna filtro, uso ID
 if (lfilter == 0){
   col <- "ID"
   low <-  args[1] %>% read.csv(sep=";") %>% as.tbl() %>% select_("ID") %>% min
@@ -40,6 +43,7 @@ if (lfilter == 0){
   }
 }
 
+### selezionare qui i dataset con i relativi filtri
 if (onlyDoe){
   ds <- args[1] %>% read.csv(sep=";") %>% as.tbl() %>% filter(type=="exact") %>%
     select_("-type")
@@ -47,42 +51,57 @@ if (onlyDoe){
   ds <- args[1] %>% read.csv(sep=";") %>% as.tbl() %>% select_("-type")
 }
 
-# absolute values in the dataset: this is to fix the scale beside of filtering, required in the project
+# estremi assoluti del dataset
 absmax <- apply(ds, 2, max)
 absmin <- apply(ds, 2, min)
 
-# first filter is without check, since we use "ID" as default
+# filtri: il primo filtro viene effettuato senza if, perche usiamo ID come default
 ds <- ds[(ds[, col] <= up) & (ds[, col] >= low), ]
-# second filter with check
+# secondo filtro: controllo se la colonna != 0
 if(lfilter2 != 0)
   ds <- ds[(ds[, col2] <= up2) & (ds[, col2] >= low2), ]
 ###
 
-ds <- rbind(ds, absmin)
-ds <- rbind(ds, absmax)
-ds <- ds %>% as.tbl()
+# dopo aver filtrato il dataset, attacco le righe con gli estremi assoluti!
+# ds <- rbind(ds, absmin)
+# ds <- rbind(ds, absmax)
+# ds <- ds %>% as.tbl()
 
-# useful to group with colors
+# variabili per creazione del range di colori
 intervals <- as.numeric(args[length(args)])
 args <- args[-length(args)] #rm group_intervals entry in args
 
+# colonna con cui raggruppare i colori
 gcol <- as.numeric(args[length(args)])
 
-mmin <- args[1] %>% read.csv(sep = ";") %>% as.tbl() %>% select_(gcol) %>% min
-mmax <- args[1] %>% read.csv(sep = ";") %>% as.tbl() %>% select_(gcol) %>% max
+mmin <- ds %>% select_(gcol) %>% min
+mmax <- ds %>% select_(gcol) %>% max
 delta <- (mmax - mmin) / intervals
-offst <- mmin %/% delta
+# calcolo l'offset come il primo intervallo, che puo essere negativo
+# l'indice di intervallo di ciascun valore sara quindi x %/% delta - offset
+# offst <- mmin %/% delta
+# fine variabili range colori
 
 png(args[13], width = as.numeric(args[11]), height= as.numeric(args[12]))
     
-  for(i in 1:nrow(ds)) intervals[i] <- ds[i, gcol] %/% delta - offst + 1  
+  # attacco la colonna degli intervalli in cui ricade il valore della colonna
+  # su cui ho scelto di raggruppare i colori
+  #for(i in 1:nrow(ds)) intervals[i] <- ds[i, gcol] %/% delta - offst + 1  
+  for(i in 1:nrow(ds)) intervals[i] <- (ds[i, gcol] - mmin) %/% delta + 1  
   ds <- cbind(ds, as.numeric(intervals))
   names(ds)[length(ds)] <- "interval"
 
   clrs <- colors()[ds$interval * 5]
-  
+
+# attacco le righe con gli estremi assoluti dopo il solo filtro sul tipo,
+# per fissare il fondo scala!
+  ds <- rbind(ds, absmin)
+  ds <- rbind(ds, absmax)
+  ds %>% as.tbl()
+
   ds <- ds %>% select_("-ID")
   ds <- ds %>% select_("-interval")
   parcoord(ds, col = clrs, var.label=TRUE)
+  #parcoord(ds, col = rainbow(nrow(ds)), var.label=TRUE)
   
 dev.off()
